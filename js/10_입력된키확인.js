47d30535c42b128eb65f8732aeaeddd2267388f7
tje0714 <tje0714@outlook.kr>
2025-07-25 16:08:47 +0900

    배열을 활용한 점심메뉴뽑기 실습

---
+++ js/10_입력된키확인.js
@@ -0,0 +1,73 @@
+// 화면에 존재하는 key 모두 가져오기
+const key = document.querySelectorAll(".key");
+
+// 특정 요소 X
+// 문서(화면 전체)에서 키가 눌러지는걸 감지했을 때
+
+document.addEventListener("keydown", function (e) {
+  // e: 이벤트 객체(이벤트 관련 정보가 모두 담겨 있음)
+  // e.key : 입력된 키
+
+  // 문자열.toLowerCase() : 영어를 모두 소문자로 변경
+  // 문자열.toUpperCase() : 영어를 모두 대문자로 변경
+  // index = 0 번 째 부터 시작 마지막은 -1 한 숫자값
+  // length = 총 글자 수 와 같이 길이 수
+
+  let idx; // 인덱스 값을 저장할 변수 이름
+
+  // 입력된 키와 일치하는 case를 실행
+
+  switch (e.key.toLowerCase()) {
+    case "q":
+      idx = 0;
+      break;
+    case "w":
+      idx = 1;
+      break;
+    case "e":
+      idx = 2;
+      break;
+    case "r":
+      idx = 3;
+      break;
+    default:
+      alert("준비중입니다.");
+      return;
+  }
+  key[idx].style.color = "#2a9d8f";
+});
+document.addEventListener("keyup", function (e) {
+  // e: 이벤트 객체(이벤트 관련 정보가 모두 담겨 있음)
+  // e.key : 입력된 키
+
+  // 문자열.toLowerCase() : 영어를 모두 소문자로 변경
+  // 문자열.toUpperCase() : 영어를 모두 대문자로 변경
+  // index = 0 번 째 부터 시작 마지막은 -1 한 숫자값
+  // length = 총 글자 수 와 같이 길이 수
+
+  let idx; // 인덱스 값을 저장할 변수 이름
+
+  // 입력된 키와 일치하는 case를 실행
+
+  switch (e.key.toLowerCase()) {
+    case "q":
+      idx = 0;
+      break;
+    case "w":
+      idx = 1;
+      break;
+    case "e":
+      idx = 2;
+      break;
+    case "r":
+      idx = 3;
+      break;
+    default:
+      alert("준비중입니다.");
+      return;
+  }
+  // key[idx].style.color = "#white";
+  key[idx].style.color = "#333";
+});
+
+//keyup 글자색 white
